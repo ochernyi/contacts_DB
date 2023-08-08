c3b71db243673af36318880a73064677a56b772c
Oleh Chornyi <ochernyi04@gmail.com>
2023-08-08 22:50:07 +0300

    Reformat test

--- tests.py
+++ tests.py
@@ -7,160 +7,61 @@ from API import search_handler
 class TestSearchHandler(unittest.TestCase):
     def setUp(self):
         self.app = Flask(__name__)
-        self.app.add_url_rule('/search', 'search_handler', search_handler, methods=['GET'])
+        self.app.add_url_rule(
+            "/search", "search_handler", search_handler, methods=["GET"]
+        )
         self.client = self.app.test_client()
 
-    @patch('DBConnection.create_connection')
+    @patch("DBConnection.create_connection")
     def test_search_handler_with_results(self, mock_create_connection):
-
         mock_cursor = Mock()
-        result = mock_cursor.fetchall.return_value = [[
-            7,
-            "artur.cmp@gmail.com",
-            "Campo",
-            "artur.cmp@gmail.com"
-        ],
-            [
-                17,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                27,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                37,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                47,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                57,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                67,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                77,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                87,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                97,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                107,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                117,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                127,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                137,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                147,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                157,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                167,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                177,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                187,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                197,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ],
-            [
-                207,
-                "artur.cmp@gmail.com",
-                "Campo",
-                "artur.cmp@gmail.com"
-            ]]
-        mock_create_connection.return_value.__enter__.return_value.cursor.return_value = mock_cursor
+        result = mock_cursor.fetchall.return_value = [
+            [7, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [17, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [27, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [37, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [47, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [57, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [67, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [77, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [87, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [97, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [107, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [117, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [127, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [137, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [147, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [157, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [167, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [177, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [187, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [197, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+            [207, "artur.cmp@gmail.com", "Campo", "artur.cmp@gmail.com"],
+        ]
+        mock_create_connection.return_value.__enter__.return_value.cursor.return_value = (
+            mock_cursor
+        )
 
-        response = self.client.get('/search?query=Campo')
+        response = self.client.get("/search?query=Campo")
         data = response.get_json()
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data, result)
 
-    @patch('DBConnection.create_connection')
+    @patch("DBConnection.create_connection")
     def test_search_handler_no_results(self, mock_create_connection):
-
         mock_cursor = Mock()
         mock_cursor.fetchall.return_value = []
-        mock_create_connection.return_value.__enter__.return_value.cursor.return_value = mock_cursor
+        mock_create_connection.return_value.__enter__.return_value.cursor.return_value = (
+            mock_cursor
+        )
 
-        response = self.client.get('/search?query=Jonny')
+        response = self.client.get("/search?query=Jonny")
         data = response.get_json()
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data, [])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
