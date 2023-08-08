e3be22e860c55718c55f71cc880acd77d7c7b954
Oleh Chornyi <ochernyi04@gmail.com>
2023-08-08 19:41:47 +0300

    Create table in DB and fill it

---
+++ main.py
@@ -0,0 +1,29 @@
+import csv
+
+from DBConnection import create_connection
+
+create_table_query = """
+CREATE TABLE contacts (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100)
+);
+"""
+with create_connection().cursor() as cursor:
+    cursor.execute(create_table_query)
+    create_connection().commit()
+
+with open("Nimble_Contacts.csv", newline="") as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader)
+    for row in csvreader:
+        first_name, last_name, email = row
+        with create_connection().cursor() as cursor:
+            cursor.execute(
+                "INSERT INTO contacts ("
+                "first_name, last_name, email"
+                ") VALUES (%s, %s, %s)",
+                (first_name, last_name, email),
+            )
+            create_connection().commit()
